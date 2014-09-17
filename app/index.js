2ef2054225efd06722b7797d39c0a06dd0912adf
Alexey Petushkov <mentatxx@gmail.com>
2014-09-17 21:36:00 +0400

    Converted to yeoman template

---
+++ app/index.js
@@ -0,0 +1,15 @@
+/*global module*/
+'use strict';
+
+var yeoman = require('yeoman-generator');
+
+module.exports = yeoman.generators.Base.extend({
+    app: function() {
+        this.copy('.bowerrc', '.bowerrc');
+        this.copy('.gitignore', '.gitignore');
+        this.copy('bower.json', 'bower.json');
+        this.directory('www', 'www');
+
+        this.bowerInstall('');
+    }
+});
