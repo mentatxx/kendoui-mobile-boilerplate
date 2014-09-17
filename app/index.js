5085c28e08db7fb02edae7516700f114026d7f88
Alexey Petushkov <mentatxx@gmail.com>
2014-09-17 23:25:24 +0400

    .gitignore renamed

--- app/index.js
+++ app/index.js
@@ -6,7 +6,7 @@ var yeoman = require('yeoman-generator');
 module.exports = yeoman.generators.Base.extend({
     app: function() {
         this.copy('.bowerrc', '.bowerrc');
-        this.copy('.gitignore', '.gitignore');
+        this.copy('gitignore', '.gitignore');
         this.copy('bower.json', 'bower.json');
         this.directory('www', 'www');
 
